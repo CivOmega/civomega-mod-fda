e3f02a86ffd2384346335bd3f5970420fce4a6d1
slifty <slifty@gmail.com>
2015-02-23 16:03:45 -0500

    Added initial question and results

--- comod_fda/parser.py
+++ comod_fda/parser.py
@@ -5,11 +5,43 @@ Must define three methods:
 * render_answer_html(answer_data)
 * render_answer_json(answer_data)
 """
-from .patterns import PATTERNS
+from .patterns import PATTERNS, FOOD_RECALL_COUNT_PATTERNS, FOOD_RECALL_DETAIL_PATTERNS
 
 import json
+import os
+import re
+import requests
 from django.template import loader, Context
-from random import Random
+from django.conf import settings
+
+# a regular expression so we can find the variables in
+# the "blah blah pattern {variable}" patterns
+PATTERN_ARGS_RE = re.compile(r'{([A-Za-z0-9_]+)}')
+
+
+def get_api_key():
+    key = None
+    try:
+        key = settings.SUNLIGHT_API_KEY
+    except:
+        pass
+    if 'FDA_API_KEY' in os.environ:
+        key = os.environ['FDA_API_KEY']
+    if key == None:
+        raise Exception("To use this module, you must have an FDA API Key.")
+    else:
+        return key
+
+def food_recall_count(food):
+    url = 'https://api.fda.gov/food/enforcement.json?api_key=%s&search=reason_for_recall:%s&limit=100' %  (get_api_key(), food)
+    resp = requests.get(url)
+    return resp.json()
+
+def food_recall_search_by_reason(reason):
+    url = 'https://api.fda.gov/food/enforcement.json?api_key=%s&search=reason_for_recall:%s&limit=100' %  (get_api_key(), reason)
+    resp = requests.get(url)
+    return resp.json()
+
 
 
 ############################################################
@@ -18,24 +50,50 @@ def answer_pattern(pattern, args):
     """
     Returns a `dict` representing the answer to the given
     pattern & pattern args.
-
-    'plaintxt' should always be a returned field
-
     """
     if pattern not in PATTERNS:
+      # not one of our patterns
       return None
     if len(args) != 1:
+      # we didn't actually search anything. (if this is a slow API, you can
+      # change this to "len(args) < 5" to wait until a certain # of letters
+      # are typed in before firing off your search to the API.)
       return None
 
-    return {
-      'plaintxt': ''
-    }
+    if pattern in FOOD_RECALL_COUNT_PATTERNS:
+      
+      topic = args[0]
+    elif pattern in FOOD_RECALL_DETAIL_PATTERNS:
+      # We might be looking up via zip code or text search, so see what
+      # pattern the user used
+      args_keys = PATTERN_ARGS_RE.findall(pattern)
+      kwargs = dict(zip(args_keys,args))
+
+      if "reason" in kwargs:
+        # a zipcode search
+        reason = kwargs['reason']
+        return {
+          'type': 'food_recall_details',
+          'reason': reason,
+          'data': food_recall_search_by_reason(reason)
+        }
+
+    return None
+
+
+
 
 ############################################################
 # Applicable module-wide
 def render_answer_html(answer_data):
-    template = loader.get_template('comod_fda/example.html')
-    return template.render(Context(answer_data))
+    # This receives what we got in `answer_pattern` and returns HTML.
+    if answer_data and answer_data.get('type', None) == "food_recall_details":
+      data = answer_data['data']
+      template = loader.get_template('comod_fda/food_recall_details.html')
+      return template.render(Context(data))
+    else:
+      # TODO: render a template for "we don't know how to handle this search
+      raise Exception
 
 def render_answer_json(answer_data):
     return json.dumps(answer_data)
--- comod_fda/patterns.py
+++ comod_fda/patterns.py
@@ -1,18 +1,23 @@
 """
 contains `PATTERNS`, defining strings that this module may respond to
-
-Wrap "blanks" in {braces}
-
-The content inside the braces dictate the type of object being entered.  This content should be lowercase, underscore_separated.
-
-e.g. 
-PATTERNS = frozenset([
-    "is {person} a {thing}?",
-    "is {person} a {thing} in {place}?",
-])
-
-
+you can set anything you want here (which you can use in `parser.py`,
+but you must set `PATTERNS`)
 """
 
-PATTERNS = frozenset([
+# patterns for returning info about bills in congress
+FOOD_RECALL_COUNT_PATTERNS = frozenset([
+    
 ])
+
+FOOD_RECALL_DETAIL_PATTERNS = frozenset([
+    "what food recalls were because of {reason}",
+])
+
+# IMPORTANT:
+#   * all questions must be unique (you can change the variable inside
+#     the pattern if the wording is otherwise identical)
+#   * the variable must be unique inside the question (you can't have
+#     "what are {person} and {person} talking about" but you can have
+#     "what are {person1} and {person2} talking about")
+
+PATTERNS = FOOD_RECALL_COUNT_PATTERNS | FOOD_RECALL_DETAIL_PATTERNS
